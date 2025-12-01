a0056c27cbcafc8c453dfa824eeb184b58d40a53
mohannad <mohannedsabryy@gmail.com>
2025-12-01 21:31:55 +0200

    modify main

--- main.py
+++ main.py
@@ -29,11 +29,6 @@ import pyfiglet
 
 
 if __name__ == "__main__":
-    # Display the "INSTAPAY" logo in ASCII art with yellow color
-    print(termcolor.colored(pyfiglet.figlet_format("INSTAPAY"), "yellow"))
-    print("hello from branch 2")
-    print("helloo from branch1")
-    print("hiiii2")
-    print("xxxxx")
+    print("hello from my Mbranch")
     # Start the application
     Main.start()
